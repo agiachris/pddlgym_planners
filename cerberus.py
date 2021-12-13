a804ccfe037a51acc059d6806c77a72c37cc0021
agiachris <christopheragia@gmail.com>
2021-12-12 17:01:29 -0800

    increased planner support
    Signed-off-by: agiachris <christopheragia@gmail.com>

--- cerberus.py
+++ cerberus.py
@@ -1,5 +1,5 @@
-"""Fast-downward planner.
-http://www.fast-downward.org/ObtainingAndRunningFastDownward
+"""Cerberus
+https://github.com/ctpelok77/fd-red-black-postipc2018
 """
 
 import re
@@ -9,48 +9,41 @@ import subprocess
 import tempfile
 from pddlgym_planners.pddl_planner import PDDLPlanner
 from pddlgym_planners.planner import PlanningFailure
+import numpy as np
+from utils import FilesInCommonTempDirectory
 
-FD_URL = "https://github.com/ronuchit/downward.git"
+DOCKER_IMAGE = 'khodeir/planutils-taskography:latest'
+ALIASES = [
+    "seq-agl-cerberus2018", # dag from coloring
+    "seq-sat-cerberus2018",
+    "seq-agl-cerberus-gl-2018", # dag greedy level
+    "seq-sat-cerberus-gl-2018",
+]
 
-
-class FD(PDDLPlanner):
-    """Fast-downward planner.
-    """
-    def __init__(self, alias_flag="--alias seq-opt-lmcut", final_flags=""):
+class Cerberus(PDDLPlanner):
+    def __init__(self, alias=ALIASES[0]):
         super().__init__()
-        dirname = os.path.dirname(os.path.realpath(__file__))
-        self._exec = os.path.join(dirname, "FD/fast-downward.py")
-        print("Instantiating FD", end=' ')
-        if alias_flag:
-            print("with", alias_flag, end=' ')
-        if final_flags:
-            print("with", final_flags, end=' ')
-        print()
-        self._alias_flag = alias_flag
-        self._final_flags = final_flags
-        if not os.path.exists(self._exec):
-            self._install_fd()
+        print(f"Instantiating Cerberus with --alias {alias}")
+        assert alias in ALIASES, f"Expected alias in {ALIASES}"
+        self.alias = alias
+        self.install_cerberus()
+
+    def install_cerberus(self):
+        subprocess.check_call(f'docker pull {DOCKER_IMAGE}', shell=True, stdout=subprocess.DEVNULL)
+
+    def plan_from_pddl(self, dom_file, prob_file, horizon=np.inf, timeout=10, remove_files=False):
+        with FilesInCommonTempDirectory(dom_file, prob_file) as (dom_file, prob_file):
+            return super().plan_from_pddl(dom_file, prob_file, horizon=horizon, timeout=timeout, remove_files=remove_files)
+
 
     def _get_cmd_str(self, dom_file, prob_file, timeout):
-        sas_file = tempfile.NamedTemporaryFile(delete=False).name
         timeout_cmd = "gtimeout" if sys.platform == "darwin" else "timeout"
-        cmd_str = "{} {} {} {} --sas-file {} {} {} {}".format(
-            timeout_cmd, timeout, self._exec, self._alias_flag, sas_file,
-            dom_file, prob_file, self._final_flags)
+        probdom_dir = os.path.dirname(dom_file)
+        dom_fname = os.path.basename(dom_file)
+        prob_fname = os.path.basename(prob_file)
+        assert probdom_dir == os.path.dirname(prob_file), "Files must be in the same directory"
+        cmd_str = f"docker run --privileged -it -v {probdom_dir}:/problem -w /problem {DOCKER_IMAGE} {timeout_cmd} {timeout} /root/.planutils/bin/cerberus --alias {self.alias} /problem/{dom_fname} /problem/{prob_fname}"
         return cmd_str
-
-    def _get_cmd_str_searchonly(self, sas_file, timeout):
-        timeout_cmd = "gtimeout" if sys.platform == "darwin" else "timeout"
-        cmd_str = "{} {} {} {} --search {} {}".format(
-            timeout_cmd, timeout, self._exec, self._alias_flag,
-            sas_file, self._final_flags)
-        return cmd_str
-
-    def _cleanup(self):
-        """Run FD cleanup
-        """
-        cmd_str = "{} --cleanup".format(self._exec)
-        subprocess.getoutput(cmd_str)
 
     def _output_to_plan(self, output):
         # Technically this is number of evaluated states which is always
@@ -58,9 +51,9 @@ class FD(PDDLPlanner):
         # with FF.
         num_node_expansions = re.findall(r"evaluated (\d+) state", output.lower())
         plan_length = re.findall(r"plan length: (\d+) step", output.lower())
-        plan_cost = re.findall(r"] plan cost: (\d+)", output.lower())
-        search_time = re.findall(r"] search time: (\d+\.\d+)", output.lower())
-        total_time = re.findall(r"] total time: (\d+\.\d+)", output.lower())
+        plan_cost = re.findall(r"plan cost: (\d+)", output.lower())
+        search_time = re.findall(r"search time: (\d+\.\d+)", output.lower())[-1:]
+        total_time = re.findall(r"total time: (\d+\.\d+)", output.lower())
         if "num_node_expansions" not in self._statistics:
             self._statistics["num_node_expansions"] = 0
         if len(num_node_expansions) == 1:
@@ -74,7 +67,7 @@ class FD(PDDLPlanner):
                 self._statistics["search_time"] = search_time_float
             except:
                 raise PlanningFailure("Error on output's search time format: {}".format(search_time[0]))
-        if len(search_time) == 1:
+        if len(total_time) == 1:
             try:
                 total_time_float = float(total_time[0])
                 self._statistics["total_time"] = total_time_float
@@ -92,21 +85,17 @@ class FD(PDDLPlanner):
                 self._statistics["plan_cost"] = plan_cost_int
             except:
                 raise PlanningFailure("Error on output's plan cost format: {}".format(plan_cost[0]))
-        if "Solution found!" not in output:
-            raise PlanningFailure("Plan not found with FD! Error: {}".format(
+        if "Solution found" not in output:
+            raise PlanningFailure("Plan not found with Cerberus! Error: {}".format(
                 output))
         if "Plan length: 0 step" in output:
             return []
         
-        fd_plan = re.findall(r"(.+) \(\d+?\)", output.lower())
-        if not fd_plan:
-            raise PlanningFailure("Plan not found with FD! Error: {}".format(
+        plan = re.findall(r"(.+) \(\d+?\)", output.lower())
+        if not plan:
+            raise PlanningFailure("Plan not found with Cerberus! Error: {}".format(
                 output))
-        return fd_plan
-
-    def _install_fd(self):
-        loc = os.path.dirname(self._exec)
-        # Install and compile FD.
-        os.system("git clone {} {}".format(FD_URL, loc))
-        os.system("cd {} && ./build.py && cd -".format(loc))
-        assert os.path.exists(self._exec)
+        return plan
+    
+    def _cleanup(self):
+        pass
--- delfi.py
+++ delfi.py
@@ -1,5 +1,5 @@
-"""Fast-downward planner.
-http://www.fast-downward.org/ObtainingAndRunningFastDownward
+"""Delfi
+https://bitbucket.org/ipc2018-classical/team23/src/ipc-2018-seq-opt/
 """
 
 import re
@@ -9,48 +9,33 @@ import subprocess
 import tempfile
 from pddlgym_planners.pddl_planner import PDDLPlanner
 from pddlgym_planners.planner import PlanningFailure
+import numpy as np
+from utils import FilesInCommonTempDirectory
 
-FD_URL = "https://github.com/ronuchit/downward.git"
-
-
-class FD(PDDLPlanner):
-    """Fast-downward planner.
-    """
-    def __init__(self, alias_flag="--alias seq-opt-lmcut", final_flags=""):
+DOCKER_IMAGE = 'khodeir/planutils-taskography:latest'
+class Delfi(PDDLPlanner):
+    def __init__(self):
         super().__init__()
-        dirname = os.path.dirname(os.path.realpath(__file__))
-        self._exec = os.path.join(dirname, "FD/fast-downward.py")
-        print("Instantiating FD", end=' ')
-        if alias_flag:
-            print("with", alias_flag, end=' ')
-        if final_flags:
-            print("with", final_flags, end=' ')
-        print()
-        self._alias_flag = alias_flag
-        self._final_flags = final_flags
-        if not os.path.exists(self._exec):
-            self._install_fd()
+        print("Instantiating Delfi")
+        self.install_delfi()
+
+    def install_delfi(self):
+        subprocess.check_call(f'docker pull {DOCKER_IMAGE}', shell=True, stdout=subprocess.DEVNULL)
+
+    def plan_from_pddl(self, dom_file, prob_file, horizon=np.inf, timeout=10, remove_files=False):
+        self.tmpdir = FilesInCommonTempDirectory(dom_file, prob_file)
+        (dom_file, prob_file) = self.tmpdir.new_fpaths
+        return super().plan_from_pddl(dom_file, prob_file, horizon=horizon, timeout=timeout, remove_files=remove_files)
+
 
     def _get_cmd_str(self, dom_file, prob_file, timeout):
-        sas_file = tempfile.NamedTemporaryFile(delete=False).name
         timeout_cmd = "gtimeout" if sys.platform == "darwin" else "timeout"
-        cmd_str = "{} {} {} {} --sas-file {} {} {} {}".format(
-            timeout_cmd, timeout, self._exec, self._alias_flag, sas_file,
-            dom_file, prob_file, self._final_flags)
+        probdom_dir = os.path.dirname(dom_file)
+        dom_fname = os.path.basename(dom_file)
+        prob_fname = os.path.basename(prob_file)
+        assert probdom_dir == os.path.dirname(prob_file), "Files must be in the same directory"
+        cmd_str = f"docker run --privileged -it -v {probdom_dir}:/problem -w /problem {DOCKER_IMAGE} {timeout_cmd} {timeout} /root/.planutils/bin/delfi /problem/{dom_fname} /problem/{prob_fname} /problem/delfi.plan --image-from-lifted-task"
         return cmd_str
-
-    def _get_cmd_str_searchonly(self, sas_file, timeout):
-        timeout_cmd = "gtimeout" if sys.platform == "darwin" else "timeout"
-        cmd_str = "{} {} {} {} --search {} {}".format(
-            timeout_cmd, timeout, self._exec, self._alias_flag,
-            sas_file, self._final_flags)
-        return cmd_str
-
-    def _cleanup(self):
-        """Run FD cleanup
-        """
-        cmd_str = "{} --cleanup".format(self._exec)
-        subprocess.getoutput(cmd_str)
 
     def _output_to_plan(self, output):
         # Technically this is number of evaluated states which is always
@@ -58,9 +43,9 @@ class FD(PDDLPlanner):
         # with FF.
         num_node_expansions = re.findall(r"evaluated (\d+) state", output.lower())
         plan_length = re.findall(r"plan length: (\d+) step", output.lower())
-        plan_cost = re.findall(r"] plan cost: (\d+)", output.lower())
-        search_time = re.findall(r"] search time: (\d+\.\d+)", output.lower())
-        total_time = re.findall(r"] total time: (\d+\.\d+)", output.lower())
+        plan_cost = re.findall(r"plan cost: (\d+)", output.lower())
+        search_time = re.findall(r"search time: (\d+\.\d+)", output.lower())[-1:]
+        total_time = re.findall(r"total time: (\d+\.\d+)", output.lower())
         if "num_node_expansions" not in self._statistics:
             self._statistics["num_node_expansions"] = 0
         if len(num_node_expansions) == 1:
@@ -74,7 +59,7 @@ class FD(PDDLPlanner):
                 self._statistics["search_time"] = search_time_float
             except:
                 raise PlanningFailure("Error on output's search time format: {}".format(search_time[0]))
-        if len(search_time) == 1:
+        if len(total_time) == 1:
             try:
                 total_time_float = float(total_time[0])
                 self._statistics["total_time"] = total_time_float
@@ -92,21 +77,22 @@ class FD(PDDLPlanner):
                 self._statistics["plan_cost"] = plan_cost_int
             except:
                 raise PlanningFailure("Error on output's plan cost format: {}".format(plan_cost[0]))
-        if "Solution found!" not in output:
-            raise PlanningFailure("Plan not found with FD! Error: {}".format(
+        if "Solution found" not in output:
+            raise PlanningFailure("Plan not found with Delfi! Error: {}".format(
                 output))
         if "Plan length: 0 step" in output:
             return []
-        
-        fd_plan = re.findall(r"(.+) \(\d+?\)", output.lower())
-        if not fd_plan:
-            raise PlanningFailure("Plan not found with FD! Error: {}".format(
+        try:
+            plan_fpath = os.path.join(self.tmpdir.dirname, 'delfi.plan')
+            with open(plan_fpath, 'r') as f:
+                plan_output = f.read()
+            self.tmpdir.cleanup()
+            plan = re.findall(r"^\(([^)]+)\)", plan_output, re.M)
+            assert plan
+            return plan
+        except:
+            raise PlanningFailure("Plan not found with Delfi! Error: {}".format(
                 output))
-        return fd_plan
-
-    def _install_fd(self):
-        loc = os.path.dirname(self._exec)
-        # Install and compile FD.
-        os.system("git clone {} {}".format(FD_URL, loc))
-        os.system("cd {} && ./build.py && cd -".format(loc))
-        assert os.path.exists(self._exec)
+    
+    def _cleanup(self):
+        pass
